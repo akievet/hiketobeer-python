c24e99624db0be393ffa141c5bd033c297570747
akievet <akievet@gmail.com>
2016-03-12 13:03:23 -0500

    renames hikes to hike model

--- hikes/models.py
+++ hikes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from datetime import date
 
-class Hikes(models.Model):
+class Hike(models.Model):
     name = models.CharField(max_length=128, default='')
     date = models.DateField(default=date.today)
     map_id = models.CharField(max_length=64, default=None)
