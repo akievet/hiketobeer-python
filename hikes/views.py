32d4fc6e1720709734c8deab71dfdd67b74ea104
akievet <akievet@gmail.com>
2016-03-12 13:09:55 -0500

    Gets hike name to display on page

--- hikes/views.py
+++ hikes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from hikes.models import Hike
 
 def home_page(request):
     return render(request, 'home.html')
 
 def view_hike(request, hike_id):
-    return render(request, 'hike.html')
+    hike = Hike.objects.get(id=hike_id)
+    return render(request, 'hike.html', {'hike': hike})
