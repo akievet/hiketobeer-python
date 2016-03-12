efa0a95d520530d0dcc833e8ef3e1cca82d7732a
akievet <akievet@gmail.com>
2016-03-12 15:07:51 -0500

    Adds python runtime

--- hiketobeer_python/settings.py
+++ hiketobeer_python/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vv!-cz3wxhn+3vlgd)l2xf(&4)441(*cpx@yhj9f4*oc_#7fo-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['peaceful-spire-20022.herokuapp.com']
 
 
 # Application definition
